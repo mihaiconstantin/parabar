aaf7be1ef9e4d1781049076c5fce8891f7bd8840
mihaiconstantin <mihai@mihaiconstantin.com>
2022-11-11 16:33:12 +0100

    Feat: add file with global constants

---
+++ R/constants.R
@@ -0,0 +1,13 @@
+# Common error messaged.
+.__ERRORS__ <- list(
+    #   - when an abstract class is instantiated
+    abstract_class = "Abstract class cannot to be instantiated.",
+
+    # Occurrences:
+    #   - when running into things that must have an override
+    not_implemented = "Abstract method is not implemented.",
+
+    # Occurrences:
+    #   - when running into things not yet developed
+    not_developed = "Not supported."
+)
