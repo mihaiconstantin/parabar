bec266b690e8dbc68f40466162483c9462645c62
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-15 23:15:40 +0100

    Feat: add helper for setting package options

--- R/Helper.R
+++ R/Helper.R
@@ -20,3 +20,21 @@ Helper$get_option <- function(option) {
     # Return the value.
     return(options[[option]])
 }
+
+# Set package option.
+Helper$set_option <- function(option, value) {
+    # Get the `Options` instance from the global options, or create a new one.
+    options <- getOption("parabar", default = Options$new())
+
+    # If the requested option is unknown.
+    if (!(option %in% ls(options))) {
+        # Throw an error.
+        Exception$unknown_package_option(option)
+    }
+
+    # Set the value.
+    options[[option]] <- value
+
+    # Set the `Options` instance in the global options.
+    options(parabar = options)
+}
