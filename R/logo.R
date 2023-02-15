03b265b2a156bc2b21107ab872aad5c0ea40ca14
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-15 14:50:50 +0100

    Refactor: move `make_logo` docs to separate file

--- R/logo.R
+++ R/logo.R
@@ -1,29 +1,4 @@
-#' @title
-#' Generate Package Logo
-#'
-#' @description
-#' This function is meant for generating or updating the logo. After running
-#' this procedure we end up with what is stored in the [`parabar::LOGO`]
-#' constant.
-#'
-#' @param template A character string representing the path to the logo
-#' template.
-#'
-#' @param version A numerical vector of three positive integers representing the
-#' version of the package to append to the logo.
-#'
-#' @return The ASCII logo.
-#'
-#' @examples
-#' # Generate the logo.
-#' logo <- make_logo()
-#'
-#' # Print the logo.
-#' cat(logo)
-#'
-#' @seealso [`parabar::LOGO`]
-#'
-#' @export
+#' @template make-logo
 make_logo <- function(template = "./inst/assets/logo/logo.txt", version = c(1, 0, 0)) {
     # Load the ASCII logo.
     logo <- readLines(template)
