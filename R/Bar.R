b797f98487a6c003bfd2c2d4e3380903723d94b7
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-16 16:00:42 +0100

    Feat: export `Bar` class

--- R/Bar.R
+++ R/Bar.R
@@ -1,33 +1,75 @@
 #' @include Exception.R
 
+#' @title
+#' Bar
+#'
+#' @description
+#' This is an abstract class that defines the pure virtual methods a concrete
+#' bar must implement.
+#'
+#' @details
+#' This class cannot be instantiated. It needs to be extended by concrete
+#' subclasses that implement the pure virtual methods.
+#'
+#' @seealso
+#' [`parabar::BasicBar`], [`parabar::ModernBar`]
+#'
+#' @export
 Bar <- R6::R6Class("Bar",
     private = list(
         .bar = NULL
     ),
 
     public = list(
+        #' @description
+        #' Create a new [`parabar::Bar`] object.
+        #'
+        #' @return
+        #' Instantiating this call will throw an error.
         initialize = function() {
             Exception$abstract_class_not_instantiable(self)
         },
 
-        # Abstract method for creating a progress bar.
+        #' @description
+        #' Create a progress bar.
+        #'
+        #' @param total The total number of times the progress bar should tick.
+        #'
+        #' @param initial The starting point of the progress bar.
+        #'
+        #' @param ... Additional arguments for the bar creation. See the
+        #' **Details** section for more information.
+        #'
+        #' @details
+        #' The optional `...` named arguments depend on the specific concrete
+        #' implementation (i.e., [`parabar::BasicBar`] or
+        #' [`parabar::ModernBar`]).
+        #'
+        #' @return
+        #' This method returns void. The resulting bar is stored in the private
+        #' field `.bar`, accessible via the active binding `engine`.
         create = function(total, initial, ...) {
             Exception$method_not_implemented()
         },
 
-        # Abstract method for updating a progress bar.
+        #' @description
+        #' Update the progress bar.
+        #'
+        #' @param current The position the progress bar should be at (e.g., 30
+        #' out of 100), usually the index in a loop.
         update = function(current) {
             Exception$method_not_implemented()
         },
 
-        # Abstract method for terminating a progress bar.
+        #' @description
+        #' Terminate the progress bar.
         terminate = function() {
             Exception$method_not_implemented()
         }
     ),
 
     active = list(
-        # Get the bar engine.
+        #' @field engine The bar engine.
         engine = function() { return(private$.bar) }
     )
 )
