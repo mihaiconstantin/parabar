ada7963691c13e7564845ee767ed60722240b26b
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-16 19:06:33 +0100

    Feat: export `BarFactory` class

--- R/BarFactory.R
+++ R/BarFactory.R
@@ -1,8 +1,50 @@
 #' @include Exception.R BasicBar.R ModernBar.R
 
-# Factory for fetching bar types of different instances.
+#' @title
+#' BackendFactory
+#'
+#' @description
+#' This is a concrete implementation of the abstract class [`parabar::Bar`]
+#' using the [`progress::progress_bar`] as engine for the progress bar.
+#'
+#' @seealso
+#' [`parabar::Bar`], [`parabar::BasicBar`], [`parabar::ModernBar`]
+#'
+#' @examples
+#' # Create a bar factory.
+#' bar_factory <- BarFactory$new()
+#'
+#' # Get a modern bar instance.
+#' bar <- bar_factory$get("modern")
+#'
+#' # Check the class of the bar instance.
+#' class(bar)
+#'
+#' # Get a basic bar instance.
+#' bar <- bar_factory$get("basic")
+#'
+#' # Check the class of the bar instance.
+#' class(bar)
+#'
+#' @export
 BarFactory <- R6::R6Class("BarFactory",
     public = list(
+        #' @description
+        #' Obtain a concrete implementation of the abstract [`parabar::Bar`]
+        #' class of the specified type.
+        #'
+        #' @param type A character string specifying the type of the
+        #' [`parabar::Bar`] to instantiate. Possible values are `"modern"` and
+        #' `"basic"`. See the **Details** section for more information.
+        #'
+        #' @details
+        #' When `type = "modern"` a [`parabar::ModernBar`] instance is created
+        #' and returned. When `type = "basic"` a [`parabar::BasicBar`] instance
+        #' is provided instead.
+        #'
+        #' @return
+        #' A concrete implementation of the class [`parabar::Bar`]. It throws an
+        #' error if the requested bar `type` is not supported.
         get = function(type) {
             return(
                 switch(type,
