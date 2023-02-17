148411e08eb61479eb7fc7a198bca49313dec7a6
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-17 19:09:50 +0100

    Feat: export `ProgressDecorator` context class

--- R/ProgressDecorator.R
+++ R/ProgressDecorator.R
@@ -1,6 +1,118 @@
 #' @include Context.R Bar.R
 
-# Blueprint for creating a progress tracking context manager for consuming backend APIs.
+#' @title
+#' ProgressDecorator
+#'
+#' @description
+#' This class represents a progress tracking context for interacting with
+#' [`parabar::Backend`] implementations via the [`parabar::Service`] interface.
+#'
+#' @details
+#' This class extends the base [`parabar::Context`] class and overrides the
+#' [`sapply`][`parabar::Context`] parent method to decorate the backend instance
+#' with additional functionality. Specifically, this class creates a temporary
+#' file to log the progress of backend tasks, and then creates a progress bar to
+#' display the progress of the backend tasks.
+#'
+#' The progress bar is updated after each backend task execution. The timeout
+#' between subsequent checks of the temporary log file is controlled by the
+#' [`parabar::Options`] class and defaults to `0.001`. This value can be
+#' adjusted via the [`parabar::Options`] instance present in the session
+#' [`base::.Options`] list (i.e., see [parabar::set_option()]). For example, to
+#' set the timeout to `0.1` we can run `set_option("progress_timeout", 0.1)`.
+#'
+#' This class is a good example of how to extend the base [`parabar::Context`]
+#' class to decorate the backend instance with additional functionality.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Define a task to run in parallel.
+#' task <- function(x, y) {
+#'     # Sleep a bit.
+#'     Sys.sleep(0.25)
+#'
+#'     # Return the result of a computation.
+#'     return(x + y)
+#' }
+#'
+#' # Create a specification object.
+#' specification <- Specification$new()
+#'
+#' # Set the number of cores.
+#' specification$set_cores(cores = 2)
+#'
+#' # Set the cluster type.
+#' specification$set_type(type = "psock")
+#'
+#' # Create a backend factory.
+#' backend_factory <- BackendFactory$new()
+#'
+#' # Get a backend instance that does not support progress tracking.
+#' backend <- backend_factory$get("sync")
+#'
+#' # Create a progress tracking context object.
+#' context <- ProgressDecorator$new()
+#'
+#' # Attempt to set the incompatible backend instance.
+#' context$set_backend(backend)
+#'
+#' # Get a backend instance that does support progress tracking.
+#' backend <- backend_factory$get("async")
+#'
+#' # Register the backend with the context.
+#' context$set_backend(backend)
+#'
+#' # From now all, all backend operations are interecepted by the context.
+#'
+#' # Start the backend.
+#' context$start(specification)
+#'
+#' # Create a bar factory.
+#' bar_factory <- BarFactory$new()
+#'
+#' # Get a modern bar instance.
+#' bar <- bar_factory$get("modern")
+#'
+#' # Register the bar with the context.
+#' context$set_bar(bar)
+#'
+#' # Configure the bar.
+#' context$configure_bar(
+#'     show_after = 0,
+#'     format = " > completed :current out of :total tasks [:percent] [:elapsed]"
+#' )
+#'
+#' # Run a task in parallel (i.e., approx. 3.125 seconds).
+#' context$sapply(x = 1:25, fun = task, y = 10)
+#'
+#' # Get the task output.
+#' backend$get_output()
+#'
+#' # Change the bar type.
+#' bar <- bar_factory$get("basic")
+#'
+#' # Register the bar with the context.
+#' context$set_bar(bar)
+#'
+#' # Remove the previous bar configuration.
+#' context$configure_bar()
+#'
+#' # Run a task in parallel (i.e., approx. 3.125 seconds).
+#' context$sapply(x = 1:25, fun = task, y = 10)
+#'
+#' # Get the task output.
+#' backend$get_output()
+#'
+#' # Close the backend.
+#' context$stop()
+#' }
+#'
+#' @seealso
+#' [`parabar::Context`], [`parabar::Service`], [`parabar::Backend`], and
+#' [`parabar::AsyncBackend`].
+#'
+#' @export
 ProgressDecorator <- R6::R6Class("ProgressDecorator",
     inherit = Context,
 
@@ -100,7 +212,16 @@ ProgressDecorator <- R6::R6Class("ProgressDecorator",
     ),
 
     public = list(
-        # Override to validate the backend provided.
+        #' @description
+        #' Set the backend instance to be used by the context.
+        #'
+        #' @param backend An object of class [`parabar::Backend`] that supports
+        #' progress tracking implements the [`parabar::Service`] interface.
+        #'
+        #' @details
+        #' This method overrides the parent method to validate the backend
+        #' provided and guarantee it is an instance of the
+        #' [`parabar::AsyncBackend`] class.
         set_backend = function(backend) {
             # Get backend type.
             type <- Helper$get_class_name(backend)
@@ -115,19 +236,42 @@ ProgressDecorator <- R6::R6Class("ProgressDecorator",
             super$set_backend(backend)
         },
 
-        # Set progress bar instance.
+        #' @description
+        #' Set the [`parabar::Bar`] instance to be used by the context.
+        #'
+        #' @param bar An object of class [`parabar::Bar`].
         set_bar = function(bar) {
             # Set the bar.
             private$.bar = bar
         },
 
         # Set bar configuration.
+        #' @description
+        #' Configure the [`parabar::Bar`] instance registered with the context.
+        #'
+        #' @param ... A list of named arguments passed to the `create()` method
+        #' of the [`parabar::Bar`] instance. See the documentation of the
+        #' specific concrete bar for details (e.g., [`parabar::ModernBar`]).
         configure_bar = function(...) {
             # Store bar options.
             private$.bar_config = list(...)
         },
 
-        # Run the task with a progress bar.
+        #' @description
+        #' Run a task on the backend akin to [parallel::parSapply()], but with a
+        #' progress bar.
+        #'
+        #' @param x A vector (i.e., usually of integers) to pass to the `fun`
+        #' function.
+        #'
+        #' @param fun A function to apply to each element of `x`.
+        #'
+        #' @param ... Additional arguments to pass to the `fun` function.
+        #'
+        #' @return
+        #' This method returns void. The output of the task execution must be
+        #' stored in the private field `.output` on the [`parabar::Backend`]
+        #' abstract class, and is accessible via the `get_output()` method.
         sapply = function(x, fun, ...) {
             # Create file for logging progress.
             log <- private$.make_log()
@@ -150,7 +294,7 @@ ProgressDecorator <- R6::R6Class("ProgressDecorator",
     ),
 
     active = list(
-        # Get the progress bar instance.
+        #' @field bar The [`parabar::Bar`] instance registered with the context.
         bar = function() { return(private$.bar) }
     )
 )
