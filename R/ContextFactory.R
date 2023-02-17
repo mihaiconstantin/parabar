f6cbfeb3265c42a70c6be83c38b159f2d087109e
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-17 20:30:37 +0100

    Feat: export `ContextFactory` class

--- R/ContextFactory.R
+++ R/ContextFactory.R
@@ -1,8 +1,48 @@
 #' @include Exception.R Context.R ProgressDecorator.R
 
 # Factory for fetching context instances (i.e., for managing and decorating backends).
+#' @title BackendFactory
+#'
+#' @description
+#' This class is a factory that provides instances of the [`parabar::Context`]
+#' class.
+#'
+#' @examples
+#' # Create a context factory.
+#' context_factory <- ContextFactory$new()
+#'
+#' # Get a regular context instance.
+#' context <- context_factory$get("regular")
+#'
+#' # Check the class of the context instance.
+#' class(context)
+#'
+#' # Get a progress context instance.
+#' context <- context_factory$get("progress")
+#' class(context)
+#'
+#' @seealso
+#' [`parabar::Context`], [`parabar::ProgressDecorator`], and
+#' [`parabar::Service`].
+#'
+#' @export
 ContextFactory <- R6::R6Class("ContextFactory",
     public = list(
+        #' @description
+        #' Obtain instances of the [`parabar::Context`] class.
+        #'
+        #' @param type A character string specifying the type of the
+        #' [`parabar::Context`] to instantiate. Possible values are `"regular"`
+        #' and `"progress"`. See the **Details** section for more information.
+        #'
+        #' @details
+        #' When `type = "regular"` a [`parabar::Context`] instance is created
+        #' and returned. When `type = "progress"` a
+        #' [`parabar::ProgressDecorator`] instance is provided instead.
+        #'
+        #' @return
+        #' An object of type [`parabar::Context`]. It throws an error if the
+        #' requested context `type` is not supported.
         get = function(type) {
             return(
                 switch(type,
