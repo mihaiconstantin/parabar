a9c18f4fd5df6c187eec6137bc9995aea2072d02
mihaiconstantin <mihai@mihaiconstantin.com>
2023-02-21 17:34:39 +0100

    Docs: merge documentations of options-related functions

--- man-roxygen/set-default-options.R
+++ man-roxygen/set-default-options.R
@@ -1,19 +1,15 @@
-#' @title
-#' Set Default Options
-#'
 #' @description
-#' This function is used to set the default [`options`][base::options()] values
-#' for the [`parabar::parabar`] package. The function is automatically called at
-#' package load and the entry created can be retrieved via
-#' [`getOption("parabar")`][base::getOption()]. Specific package
+#' The [parabar::set_default_options()] function is used to set the default
+#' [`options`][base::options()] values for the [`parabar::parabar`] package. The
+#' function is automatically called at package load and the entry created can be
+#' retrieved via [`getOption("parabar")`][base::getOption()]. Specific package
 #' [`options`][base::options()] can be retrieved using the helper function
 #' [parabar::get_option()].
 #'
 #' @return
-#' The function returns void. The [`options`][base::options()] set can be
-#' consulted via the [`base::.Options`] list. See the [`parabar::Options`]
-#' [`R6::R6`] class for more information on the default values set by this
-#' function.
+#' The [parabar::set_default_options()] function returns void. The
+#' [`options`][base::options()] set can be consulted via the [`base::.Options`]
+#' list. See the [`parabar::Options`] [`R6::R6`] class for more information on
+#' the default values set by this function.
 #'
-#' @seealso
-#' [`parabar::Options`], [parabar::get_option()]
+#' @rdname option
--- man-roxygen/set-option.R
+++ man-roxygen/set-option.R
@@ -1,22 +1,23 @@
 #' @title
-#' Set Package Option
+#' Get or Set Package Option
 #'
 #' @description
-#' This function is a helper for setting [`parabar::parabar`]
-#' [`options`][base::options()]. The function adjusts the fields of the
-#' [`parabar::Options`] instance stored in the [`base::.Options`] list. If no
-#' [`parabar::Options`] instance is present in the [`base::.Options`] list, a
-#' new one is created.
+#' The [parabar::set_option()] function is a helper for setting
+#' [`parabar::parabar`] [`options`][base::options()]. The function adjusts the
+#' fields of the [`parabar::Options`] instance stored in the [`base::.Options`]
+#' list. If no [`parabar::Options`] instance is present in the
+#' [`base::.Options`] list, a new one is created.
 #'
 #' @param option A character string representing the name of the option to
-#' adjust. See the public fields of [`R6::R6`] class [`parabar::Options`] for
-#' the list of available [`parabar::parabar`] [`options`][base::options()].
+#' retrieve or adjust. See the public fields of [`R6::R6`] class
+#' [`parabar::Options`] for the list of available [`parabar::parabar`]
+#' [`options`][base::options()].
 #'
 #' @param value The value to set the [`option`][`parabar::Options`] to.
 #'
 #' @return
-#' The function returns void. It throws an error if the requested
-#' [`option`][`parabar::Options`] to be adjusted is not known.
+#' The [parabar::set_option()] function returns void. It throws an error if the
+#' requested [`option`][`parabar::Options`] to be adjusted is not known.
 #'
 #' @examples
 #' # Get the status of progress tracking.
@@ -28,6 +29,14 @@
 #' # Get the status of progress tracking again.
 #' get_option("progress_track")
 #'
+#' # Restore default options.
+#' set_default_options()
+#'
+#' # Get the status of progress tracking yet again.
+#' get_option("progress_track")
+#'
 #' @seealso
-#' [`parabar::Options`], [parabar::get_option()],
-#' [parabar::set_default_options()]
+#' [`parabar::Options`], [parabar::set_default_options()], [base::options()],
+#' and [base::getOption()].
+#'
+#' @rdname option
