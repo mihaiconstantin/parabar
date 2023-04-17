4df3224a8c045fa66f6bc85f7c9506e9554c913e
mihaiconstantin <mihai@mihaiconstantin.com>
2023-04-17 21:03:23 +0200

    Test: add helper to convert warnings and exceptions

---
+++ tests/testthat/helpers.R
@@ -0,0 +1,15 @@
+# Helpers for testing.
+
+# Helper for extracting the message associated with errors and warnings.
+as_text <- function(expression) {
+    # Capture message.
+    message <- tryCatch(expression,
+        # Capture warning message.
+        warning = function(w) w$message,
+
+        # Capture error message.
+        error = function(e) e$message
+    )
+
+    return(message)
+}
