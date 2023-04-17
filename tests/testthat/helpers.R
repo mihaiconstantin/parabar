5927eae5f7e25b7156072e04f6856962e04177f8
mihaiconstantin <mihai@mihaiconstantin.com>
2023-04-17 21:09:07 +0200

    Test: add test helper for the `Specification` class

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -13,3 +13,27 @@ as_text <- function(expression) {
 
     return(message)
 }
+
+# Helper for testing private methods of `Specification` class.
+SpecificationTester <- R6::R6Class("SpecificationTester",
+    inherit = Specification,
+
+    private = list(
+        # Overwrite the private `.get_available_cores()` method.
+        .get_available_cores = function() {
+            return(self$available_cores)
+        }
+    ),
+
+    public = list(
+        available_cores = NULL
+    ),
+
+    active = list(
+        # Expose the private `.determine_usable_cores` method.
+        usable_cores = function() {
+            # Compute and return the number of the usable cores.
+            return(private$.determine_usable_cores(self$available_cores))
+        }
+    )
+)
