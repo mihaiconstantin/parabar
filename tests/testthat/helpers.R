62ff0eb7b1aef34ff356b7c00ce698120c6dd3de
mihaiconstantin <mihai@mihaiconstantin.com>
2023-05-05 14:19:27 +0200

    Test: add tests for progress tracking task decoration

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -68,6 +68,17 @@ task_is_running <- function(backend) {
     return(status)
 }
 
+# Check if the body of a decorated tasks contains a specific pattern.
+body_contains <- function(task, pattern, position = 2) {
+    # Get the body of the decorated task at a specific position.
+    extraction <- as.list(body(task))[position]
+
+    # Check if the body at position contains the pattern.
+    contains <- grepl(pattern, extraction)
+
+    return(contains)
+}
+
 #endregion
 
 
